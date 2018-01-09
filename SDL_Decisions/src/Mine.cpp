cdf722080523f6dad1dcfac5b8e09bad55eb606b
Pau Morales Torrent <pau.morales.torrent@gmail.com>
2018-01-09 18:42:12 +0100

    Mine.cpp

---
+++ SDL_Decisions/src/Mine.cpp
@@ -0,0 +1,26 @@
+#include "Mine.h"
+
+Mine::Mine()
+{
+	std::cout << "pene23213" << std::endl;
+}
+
+Mine::~Mine()
+{
+
+}
+
+void Mine::Update()
+{
+	std::cout << "pene" << std::endl;
+}
+
+void Mine::Enter()
+{
+
+}
+
+void Mine::Exit()
+{
+
+}
